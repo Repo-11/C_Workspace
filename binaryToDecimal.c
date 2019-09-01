86db6bf582bfb57144dc2c81ce9dcc25e1ddb405
Kallal Bhakta <44457860+K2Bhakta@users.noreply.github.com>
2019-09-01 21:01:01 +0530

    Create binaryToDecimal.c

---
+++ binaryToDecimal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int blNum, decNum = 0, rem, count = 0;
+    printf("Enter a boolean number :");
+    scanf("%d", &blNum);
+    while(blNum > 0)
+    {
+        rem = blNum%10;
+        decNum += rem*pow(2,count++);
+        //count++;
+        blNum /= 10;
+    }
+    printf("\nThe decimal number is %d\n", decNum);
+    return 0;
+}

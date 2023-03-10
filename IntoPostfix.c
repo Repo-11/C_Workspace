7ff77f7d300e16a176982d6df596646ca4f8b74d
Kallal Bhakta <44457860+Repo-11@users.noreply.github.com>
2023-03-10 10:22:13 +0530

    Add files via upload

---
+++ IntoPostfix.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define SIZE 40
+
+char stack[SIZE];
+int top = -1;
+
+int push(char ch)
+{
+    if (top == SIZE - 1)
+    {
+        printf("Stack Overflow\n");
+        exit(1);
+    }
+    else
+    {
+        top += 1;
+        stack[top] = ch;
+    }
+}
+
+void pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    else
+        top -= 1;
+}
+
+int precedence(char ch)
+{
+    switch (ch)
+    {
+    case '(':
+    case ')':
+        return 4;
+    case '^':
+        return 3;
+    case '*':
+    case '%':
+    case '/':
+        return 2;
+    case '+':
+    case '-':
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+void infixToPostfix(char infix[], char postfix[])
+{
+    int i, len, flag = 0, j;
+    push('(');
+    strcat(infix, ")");
+    len = strlen(infix);
+    for (i = 0; i < len; i++)
+        if (infix[i] == ' ' || infix[i] == '\t')
+            continue;
+        else if (isalnum(infix[i]))
+            postfix[flag++] = infix[i];
+        else if (infix[i] == '(')
+            push(infix[i]);
+        else if (infix[i] == ')')
+        {
+            j = top;
+            while (stack[j] != '(')
+            {
+                postfix[flag++] = stack[j--];
+                pop();
+            }
+            if(stack[j]=='('){
+                pop();
+                continue;
+            }
+        }
+        else if (precedence(infix[i]) > precedence(stack[top]))
+            push(infix[i]);
+        else if (precedence(infix[i]) < precedence(stack[top]))
+        {
+            while (precedence(infix[i]) <= precedence(stack[top]) && stack[top] != '(')
+            {
+                postfix[flag++] = stack[top];
+                pop();
+            }
+            push(infix[i]);
+        }
+        else if ((precedence(infix[i]) == precedence(stack[top])))
+        {
+            postfix[flag++]=stack[top];
+            pop();
+            push(infix[i]);
+        }
+    postfix[flag] = '\0';
+}
+
+int main()
+{
+    char infix[SIZE] = "K+L-M*N+(O^P)*W/U/V*T+Q";
+    char postfix[SIZE];
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression : ");
+    printf("%s\n", postfix);
+    return 0;
+}
